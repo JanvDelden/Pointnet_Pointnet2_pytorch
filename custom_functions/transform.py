142c60cb17045e0ce15beea673c781193498e1d3
dernameistegal <tester.97test@gmail.com>
2022-01-20 11:57:33 +0100

    change pars for random jutter

--- custom_functions/transform.py
+++ custom_functions/transform.py
@@ -112,7 +112,7 @@ class RandomJitter(object):
     Shifts all points a random distance. The distance shifted is different for every point
     """
 
-    def __init__(self, sigma=0.03, clip=0.06):
+    def __init__(self, sigma=0.05, clip=0.12):
         self.sigma = sigma
         self.clip = clip
 
